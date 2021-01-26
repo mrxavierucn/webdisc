4c52e6a138a0737e81fe9d8a47d7efb6e96321d4
Eduardo <edualvsald@hotmail.com>
2021-01-26 10:23:56 -0300

    fix: reparada validación del correo al registrar usuario

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
@@ -25,7 +26,7 @@ class CreateNewUser implements CreatesNewUsers
             'name' => ['required', 'string', 'max:255'],
             'rut' => ['required', 'string', 'max:255'],
             'rol' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required','string','email','max:255',Rule::unique(User::class),],
             'password' => $this->passwordRules(),
         ])->validate();
 
